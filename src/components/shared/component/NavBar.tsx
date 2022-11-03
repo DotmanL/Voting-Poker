390d9930edef6d3c93494fc3d5812b190626c8d8
Lawal Oladotun <oladotunlawal7@gmail.com>
2022-11-03 09:33:53 +0000

    Update NavBar.tsx

--- src/components/shared/component/NavBar.tsx
+++ src/components/shared/component/NavBar.tsx
@@ -38,7 +38,8 @@ export const NavBar = (props: Props) => {
   // };
 
   const handleSignOut = () => {
-    localStorage.removeItem('user');
+    // localStorage.removeItem('user');
+    localStorage.clear();
     navigate('/new-room')
     toast.info('Kindly join a room')
   }
